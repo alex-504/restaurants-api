9a022e43be1cd2bbfef701cb6aff190588664717
Alexandre Vieira <alexandre.ealimentos@gmail.com>
2021-01-22 14:23:55 +0900

    create json view

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  namespace :api, defaults: { format: :json }  do
+  namespace :api, defaults: { format: :json } do
     namespace :v1 do
       resources :restaurants, only: [ :index ]
     end
